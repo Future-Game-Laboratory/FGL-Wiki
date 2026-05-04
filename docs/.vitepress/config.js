46ebe0c218b7c61e1ceb5587f88912c553bbf260
cloud-oc <cloud-oc@users.noreply.github.com>
2026-05-04 01:44:27 +0000

    deploy: 2e4b32df8b8ff7601a594b52a9b0394ac54d792c

--- docs/.vitepress/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  title: "FGL Wiki",
-  description: "项目知识库",
-  themeConfig: {
-    logo: "",
-    nav: [{ text: "主页", link: "/" }],
-    sidebar: [{ text: "指南", items: [{ text: "首页", link: "/" }] }],
-  },
-};
